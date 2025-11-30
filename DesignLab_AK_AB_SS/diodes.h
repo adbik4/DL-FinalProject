113b3f82856f7b5d8921e311efb29a872fb1d5cd
SebastianSko <sjaksebastian@gmail.com>
2025-11-30 22:18:12 +0100

    sensors code

---
+++ DesignLab_AK_AB_SS/diodes.h
@@ -0,0 +1,18 @@
+/*
+ * diodes.h
+ *
+ * Created: 04/11/2025 15:05:56
+ *  Author: Adrian
+ */ 
+
+
+#ifndef DIODES_H_
+#define DIODES_H_
+
+void diode_pb0_on(void);
+void diode_pb0_off(void);
+void diode_pb1_on(void);
+void diode_pb1_off(void);
+
+
+#endif /* DIODES_H_ */
